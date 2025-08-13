6c152475a133a3b805690595a17082ad3036cd4d
xia1990 <sweetyx1990@gmail.com>
2025-08-13 01:40:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import os
-from flask import (
-    Flask, render_template, request, redirect, url_for, session, flash
-)
+from flask import (Flask, render_template, request, redirect, url_for, session, flash)
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
